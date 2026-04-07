25a8d67c5dc1cfb70dfb2cff20c9ec8c8961aa6c
Nikhil Gusain <159279228+nikhilgusain@users.noreply.github.com>
2026-04-07 17:35:53 +0530

    Time: 67 ms (63.36%), Space: 19.6 MB (82.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,24 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:   
-        
-        l,r = 0,0
-        dt = Counter(t)
-        ds = dict()
-        valid = 0
+    def minWindow(self, s: str, t: str) -> str:
+        td = Counter(t)
+        sd = dict()
+        valid = 0 
+        l = 0
         i,j = -1,-1
-        min_len = float('inf')
-        
-        while r<len(s):
+        best_len = float('inf')
+        for r in range(len(s)):
             ch = s[r]
-            if ch in dt:
-                ds[ch] = ds.get(ch,0)+1
-                if ds[ch] == dt[ch]:
-                    valid+=1
-            
-            while valid == len(dt):
-                if (r-l+1)<min_len:
-                    min_len = r-l+1
-                    i=l
-                    j=r
-                if s[l] in ds:
-                    ds[s[l]]-=1
-                    if ds[s[l]]<dt[s[l]]:
-                        valid-=1
+            sd[ch] = sd.get(ch,0)+1
+            if ch in td and sd[ch] == td[ch]:
+                valid+=1
+            while valid == len(td):
+                c = s[l]
+                if (r-l+1) < best_len:
+                    best_len = r-l+1
+                    i,j = l,r+1
+                
+                sd[c]-=1
+                if c in td and sd[c]<td[c]:
+                    valid-=1
                 l+=1
-                            
-            r+=1
-
-        return s[i:j+1] if i != -1 else ""
-
-            
-
-
+        return s[i:j]
